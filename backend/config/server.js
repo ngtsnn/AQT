9a59b3916bf719018f59b71d84f7f97434a762d6
ngtsnn <tusocnau@gmail.com>
2021-11-30 00:02:58 +0700

    backend setup

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7644fad5fa61b35998c02062444e724b'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'aab08744-b57a-41cc-8bbc-d3fd37c14a03'
+};
